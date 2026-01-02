492d9229a233ac5ca7ba1c58c0c832241e722036
Nitisha Agarwal <nitishaagarwal3@gmail.com>
2026-01-02 22:00:11 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+class StringLengthComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String s1, String s2) {
+        return s1.length() - s2.length();
+    }
+}
+
+class MyComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2 - o1;
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<Integer> ls = new ArrayList<>();
+        ls.add(9);
+        ls.add(4);
+        ls.add(16);
+
+        ls.sort(new MyComparator());
+        System.out.println(ls);
+
+        List<String> words = Arrays.asList("banana", "apple", "date");
+        words.sort(new StringLengthComparator());
+        System.out.println(words);
     }
 }
